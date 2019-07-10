d7dbc832258d10fba638ab43b2739d9ce1a3ee97
Vladas Zakrevskis <146100@gmail.com>
2019-07-10 13:24:47 +0300

    linux setup

--- deploy.py
+++ deploy.py
@@ -10,10 +10,9 @@ is_linux   = platform.system() == "Linux"
 python_cmd = "py "  if is_windows else "python3 "
 pip_cmd    = "pip " if is_windows else "pip3 "
 
-home = os.path.expanduser("~/")
-deps_path = home + ".deps/"
-build_tools_path = deps_path + "build_tools/"
-shell_path = home + ".shell/"
+home             = os.path.expanduser("~/")
+build_tools_path = home + ".deps/build_tools/"
+shell_path       = home + ".shell/"
 
 def run(string):
     print(string)
@@ -24,17 +23,28 @@ def run(string):
 def clone(rep, destination = ""):
     run("git clone --recursive https://github.com/vladasz/" + rep + " " + destination)
 
+def linux_setup():
+    print("Linux setup")
+    run("sudo apt install python3-pip")
+    run("sudo apt install python3-setuptools")
+    run("sudo pip3 install conan")
+
+def windows_setup():
+    print("Windows setup")
+
+def mac_setup():
+    print("Mac setup")
+    
 clone(".shell",      shell_path)
 clone("build_tools", build_tools_path)
 
-os.environ["PATH"]       += shell_path
-os.environ["PYTHONPATH"]  = build_tools_path
-os.environ["PYTHON3PATH"]  = build_tools_path
+if is_windows:
+    windows_setup()
+else if is_mac:
+    mac_setup()
+else if is_linux:
+    linux_setup()
+else:
+    print("Unknown os")
 
-run("export PYTHONPATH=${PYTHONPATH}:" + build_tools_path)
-run("echo ${PYTHONPATH}")
-run("echo ${PYTHON3PATH}")
-run("echo ${PATH}")
-run("ls " + build_tools_path)
-
-print(home)
+    
