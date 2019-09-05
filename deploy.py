f9260ab3ba239121c11a7aeac6bdad4c54bd12d0
Vladas <146100@gmail.com>
2019-09-05 23:45:09 +0300

    added ios conan profile

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import platform
 
 is_windows = platform.system() == "Windows"
@@ -11,6 +12,8 @@ home             = os.path.expanduser("~/")
 build_tools_path = home + ".deps/build_tools/"
 shell_path       = home + ".shell/"
 
+print("HELLOPOPPPP")
+
 def run(string):
     print(string)
     if os.system(string):
@@ -18,25 +21,29 @@ def run(string):
         exit()
 
 def clone(rep, destination = ""):
-    run("git clone --recursive https://github.com/vladasz/" + rep + " " + destination)
+	if not os.path.exists(destination):
+		run("git clone --recursive https://github.com/vladasz/" + rep + " " + destination)
+
+def copy_conan_profile():
+	shutil.copyfile("./deploy/conan/ios", home + ".conan/profiles/ios")
 
 def linux_setup():
     print("Linux setup")
+    copy_conan_profile()
     run("sudo apt install python3-pip")
     run("sudo apt install python3-setuptools")
     run("sudo pip3 install conan")
 
 def windows_setup():
     print("Windows setup")
+    copy_conan_profile()
     run("git submodule update --init --recursive")
     run("pip install conan")
 
 def mac_setup():
     print("Mac setup")
+    copy_conan_profile()
     run("pip3 install conan")
-    
-clone(".shell",      shell_path)
-clone("build_tools", build_tools_path)
 
 if is_windows:
     windows_setup()
@@ -46,5 +53,6 @@ elif is_linux:
     linux_setup()
 else:
     print("Unknown os")
-
     
+clone(".shell",      shell_path)
+clone("build_tools", build_tools_path)
